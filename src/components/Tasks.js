c2c63bdb6931962bd2a5fb2d4c9d7dbe800bcde8
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-08 19:48:33 +0530

    Learning Reactjs

---
+++ src/components/Tasks.js
@@ -0,0 +1,36 @@
+const task = [
+    {
+        id: 1,
+        Text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+
+    },
+    {
+        id: 2,
+        Text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+
+    },
+    {
+        id: 3,
+        Text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+
+    },
+
+
+
+]
+
+const tasks = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default tasks
